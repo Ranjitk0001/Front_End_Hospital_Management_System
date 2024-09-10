5484f32d2666242520d85271d8cb6b3ca0f20dfe
rk <ranjitkale0001@gmail.com>
2024-09-10 15:57:00 +0530

    Connect to database
    Create Component ,Service,Class
    
    MySql+Spring Boot +Angular

---
+++ src/app/patient.ts
@@ -0,0 +1,10 @@
+export class Patient {
+    id:number=0;
+    name:string=" ";
+    age:number=0;
+    blood:string="";
+    prescription:string="";
+    dose:string="";
+    fees:string="";
+    urgency:string="";
+}
