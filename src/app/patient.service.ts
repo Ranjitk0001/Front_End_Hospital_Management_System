7bf1c359bdf6e8d73d6d637e84dc528523608aa1
rk <ranjitkale0001@gmail.com>
2024-09-10 16:09:13 +0530

    both are similiers

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -9,10 +9,11 @@ import { Patient } from './patient';
 export class PatientService {
 
   constructor(private httpClient:HttpClient) {}
-  //  private baseUrl="http://localhost:8084/api/v1"
-   private baseUrl:string="http://localhost:8084/api/v1";
+   private baseUrl="http://localhost:8084/api/v1"
+  //  private baseUrl:string="http://localhost:8084/api/v1";
 
    getPatientList ():Observable<Patient[]>{
-    return this.httpClient.get<Patient[]>(this.baseUrl)
+    // return this.httpClient.get<Patient[]>(this.baseUrl)
+    return this.httpClient.get<Patient[]>(`${this.baseUrl}`)
    }
 }
